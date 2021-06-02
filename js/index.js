d81bce84a133d845833abc5f89ee9dc838051653
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-02 10:42:41 +0200

    funcion obtener elemento completada

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const miAppId = "0da24496";
+const miAppKey = "2f7e95a9192a0bb024c57856f972df7e";
+
+const obtenerElemento = (clase) => document.querySelector(`.${clase}`);
+
+fetch(
+  `https://api.tmb.cat/v1/ibus/stops/2775?app_id=${miAppId}&app_key=${miAppKey}`
+)
+  .then((response) => response.json())
+  .then((datos) => printarPagina(datos));
+
+const printarPagina = () => {
+  const parrafo = obtenerElemento("datos-json");
+  console.log(parrafo);
+};
